c41bb6658e850a29ea6d1f2d82bdce078ffd2157
jihanramadhanti.flip <jihan.ramadhanti@flip.id>
2023-02-07 00:01:39 +0700

    fix package json add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import 'nprogress/nprogress.css';
 import App from 'src/App';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
 import * as serviceWorker from 'src/serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <HelmetProvider>
@@ -19,3 +20,8 @@ ReactDOM.render(
 );
 
 serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
